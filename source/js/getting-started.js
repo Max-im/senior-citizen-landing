70ea5f85f571824f13b578d833f1ac9c13d5fe88
Max Pozhidayev <pogidaevmo@gmail.com>
2017-12-16 00:16:11 +0200

    make main menu block

--- source/js/getting-started.js
+++ source/js/getting-started.js
@@ -1,3 +1,23 @@
 console.info("Welcome to Pattern Library: Webpack Edition");
 console.info("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
 console.info("Place any generic javascript files in the ./source/js folder, and webpack will bundle it up for you.");
+
+window.addEventListener('resize', e => {
+  const width = window.innerWidth;
+
+  if( width > 768 ) {
+    document.querySelector('.mainMenu')
+      .classList.remove('hide');
+  }
+})
+
+
+document.querySelector('.header__sendwich')
+  .addEventListener('click', mainMenuToggle);
+
+
+
+function mainMenuToggle(e) {
+  document.querySelector('.mainMenu')
+    .classList.toggle('hide');
+}
--- source/js/sassLoader.js
+++ source/js/sassLoader.js
@@ -1,1 +1,5 @@
-import '../css/sass/test.scss';
+import '../css/sass/common.scss';
+import '../css/sass/main-menu.scss';
+import '../css/sass/aboutBlock.scss';
+import '../css/sass/photoBlock.scss';
+import '../css/sass/header.scss';
