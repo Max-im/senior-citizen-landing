4dd28c66b51719a40a65f24abc25e728a4ea3fa6
Max Pozhidayev <pogidaevmo@gmail.com>
2017-12-20 19:36:28 +0200

    remake inner css, fix mail btn

--- source/js/sassLoader.js
+++ source/js/sassLoader.js
@@ -1,10 +1,10 @@
 
 import '../css/sass/common.scss';
-import '../css/sass/header.scss';
+import '../css/sass/headerBlock.scss';
 import '../css/sass/aboutBlock.scss';
 import '../css/sass/photoBlock.scss';
 import '../css/sass/momentBlock.scss';
 import '../css/sass/flowersBlock.scss';
 import '../css/sass/mailBlock.scss';
-import '../css/sass/footer.scss';
-import '../css/sass/footerBottom.scss';
+import '../css/sass/footerBlock.scss';
+import '../css/sass/footerBottomBlock.scss';
